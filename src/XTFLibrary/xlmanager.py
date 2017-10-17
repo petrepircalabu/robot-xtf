0199cfde7c27ed92547565e5f4998bb9cd4837e5
Petre Pircalabu <ppircalabu@bitdefender.com>
2017-10-17 15:44:53 +0300

    SQUASHME: Add XLExceptin class
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

--- src/XTFLibrary/xlmanager.py
+++ src/XTFLibrary/xlmanager.py
@@ -1,5 +1,9 @@
 from subprocess import Popen, PIPE, call as subproc_call
 
+class XLException(Exception):
+    """ Errors relating to xtf-runner itself """
+
+# TODO: Add base class
 class XLManager:
 
     def __init__(self):
@@ -11,4 +15,6 @@ class XLManager:
         create = Popen(cmd, stdout = PIPE, stderr = PIPE)
         _, stderr = create.communicate()
 
+        if create.returncode:
+            raise XLException("Failed to create VM", stderr)
 

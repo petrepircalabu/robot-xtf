29305c64703bf524e828523ef547f4e8b2b890cb
Petre Pircalabu <ppircalabu@bitdefender.com>
2017-10-27 20:01:58 +0300

    SQUASHME: Switch to dynamic keywords
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

--- src/XTFLibrary/__init__.py
+++ src/XTFLibrary/__init__.py
@@ -1,1 +1,24 @@
-from .library import XTFLibrary
+from .xlmanager import XTFManager
+
+from robot.utils import secs_to_timestr, timestr_to_secs
+
+class XTFLibrary(object):
+
+    def __init__(self, timeout = 10.0):
+        self.keywords
+        self.timeout = timestr_to_secs(timeout)
+
+    def run_keyword(self, name, args, kwargs):
+        pass
+
+    def get_keyword_argument(self, name):
+        pass
+
+    def get_keyword_tags(self, name):
+        pass
+
+    def get_keyword_documentation(self, name):
+        pass
+
+    def get_keyword_names(self):
+        run sorted(self.keywords)
